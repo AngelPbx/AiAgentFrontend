2910cb961f1d7342f17079aa8c4ab4c47ade3111
Biplab Mondal <biplab.mondal@webviotechnologies.com>
2025-04-03 17:58:49 +0530

    implement call flow

--- src/components/custom/ReactFlow/Reactflow.jsx
+++ src/components/custom/ReactFlow/Reactflow.jsx
@@ -8,28 +8,39 @@ import {
   useEdgesState,
   addEdge,
   BackgroundVariant,
+  Panel,
 } from "@xyflow/react";
 
 import "@xyflow/react/dist/style.css";
-import PaymentInit from "./PaymentInit";
 import { initialEdges, initialNodes } from "./workflow.constants";
-import PaymentCountry from "./PaymentCountry";
-import PaymentProvider from "./PaymentProvider";
+import CustomEdge from "./CustomEdge";
+import CallBegin from "./customFeatures/CallBegin";
+import Conversation from "./customFeatures/Conversation";
+import Function from "./customFeatures/Function";
+import TransferCall from "./TransferCall";
+import CallEnd from "./customFeatures/CallEnd";
+import ConversationOptions from "./ConversationOptions";
+import CallTransfer from "./customFeatures/CallTransfer";
+import PressDigit from "./customFeatures/PressDigit";
+// import DatabaseSchemaDemo from "./Retail/DatabaseSchemaDemo";
 
 const nodeType = {
-  paymentInit: PaymentInit,
-  paymentCountry: PaymentCountry,
-  paymentProvider: PaymentProvider,
+  callBegin: CallBegin,
+  callEnd: CallEnd,
+  conversation: Conversation,
+  function: Function,
+  transferCall: TransferCall,
+  callTransfer: CallTransfer,
+  pressDigit: PressDigit,
+};
+
+const edgeType = {
+  customEdge: CustomEdge,
 };
 
 const Reactflow = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
-  //   const onConnect = useCallback(
-  //     (params) => setEdges((eds) => addEdge(params, eds)),
-  //     [setEdges]
-  //   );
 
   const onConnect = useCallback(
     (connection) => {
@@ -37,6 +48,7 @@ const Reactflow = () => {
         ...connection,
         animated: true,
         id: `e${connection.source}-${connection.target}`,
+        type: "customEdge",
       };
       setEdges((eds) => addEdge(edge, eds));
     },
@@ -44,20 +56,31 @@ const Reactflow = () => {
   );
 
   return (
-    <ReactFlow
-      nodes={nodes}
-      edges={edges}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      className="text-black font-bold"
-      nodeTypes={nodeType}
-      //   fitView
-    >
-      {/* <MiniMap /> */}
-      <Controls />
-      <Background variant={BackgroundVariant.Dots} />
-    </ReactFlow>
+    <div className="w-full h-9/10">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        className="text-black font-bold"
+        nodeTypes={nodeType}
+        edgeTypes={edgeType}
+        fitView
+      >
+        <Panel
+          position="top-left"
+          className="bg-white rounded-lg shadow-lg p-4"
+        >
+          <div className="flex flex-col gap-4">
+            <ConversationOptions />
+            {/* <PaymentProviderSelect /> */}
+          </div>
+        </Panel>
+        <Controls />
+        <Background variant={BackgroundVariant.Dots} />
+      </ReactFlow>
+    </div>
   );
 };
 
--- src/components/custom/ReactFlow/workflow.constants.js
+++ src/components/custom/ReactFlow/workflow.constants.js
@@ -1,34 +1,50 @@
+import { v4 as uuid4 } from "uuid";
 export const initialNodes = [
   {
-    id: "1",
-    position: { x: 0, y: 100 },
-    data: { amount: "100" },
-    type: "paymentInit",
+    id: "7",
+    position: { x: 275, y: 200 },
+    type: "callBegin",
   },
-  {
-    id: "2",
-    position: { x: 50, y: 100 },
-    data: { country: "United State", code: "US", currency: "$" },
-    type: "paymentCountry",
-  },
-  {
-    id: "3",
-    position: { x: 80, y: 150 },
-    data: { country: "India", code: "IN", currency: "₹" },
-    type: "paymentCountry",
-  },
-  {
-    id: "4",
-    position: { x: 110, y: 300 },
-    data: { name: "stripe", code: "st" },
-    type: "paymentProvider",
-  },
-  {
-    id: "5",
-    position: { x: 110, y: 400 },
-    data: { name: "paypal", code: "py" },
-    type: "paymentProvider",
-  },
+  // {
+  //   id: "8",
+  //   position: { x: 275, y: 400 },
+  //   type: "conversation",
+  //   data: {
+  //     label: "Conversation",
+  //     description: "This is some random description",
+  //     schema: [
+  //       { id: uuid4(), title: "First", type: "varchar" },
+  //       { id: uuid4(), title: "Second", type: "varchar" },
+  //     ],
+  //   },
+  // },
+  // {
+  //   id: "9",
+  //   position: { x: 275, y: 600 },
+  //   type: "function",
+  //   data: {
+  //     label: "Function",
+  //     description: "This is some random description",
+  //     schema: [
+  //       { id: uuid4(), title: "First", type: "varchar" },
+  //       { id: uuid4(), title: "Second", type: "varchar" },
+  //     ],
+  //   },
+  // },
+  // {
+  //   id: "10",
+  //   position: { x: 275, y: 800 },
+  //   type: "transferCall",
+  //   data: {
+  //     label: "Transfer Call",
+  //     description: "This is some random description",
+  //   },
+  // },
+  // {
+  //   id: "11",
+  //   position: { x: 275, y: 300 },
+  //   type: "callEnd",
+  // },
 ];
 
 export const initialEdges = [];
